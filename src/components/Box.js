153b50fd24978d655e73d4533cd5b0b38364037d
kumarabhishekbansal <kumarabhishekbansal>
2024-04-01 09:49:16 +0530

    adding 360 degree parano,ic view

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,17 +1,23 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
 
 export default function Box({ rotate = false }) {
   const mesh = useRef();
+  const [hovered, setHovered] = useState(false);
   useFrame(() => {
     if (rotate) {
       mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
     }
   });
   return (
-    <mesh scale={2} ref={mesh}>
+    <mesh
+      scale={2}
+      ref={mesh}
+      onPointerOver={(e) => setHovered(true)}
+      onPointerOut={(e) => setHovered(false)}
+    >
       <boxGeometry />
-      <meshStandardMaterial color="blue" />
+      <meshStandardMaterial color={hovered ? "purple" : "blue"} />
     </mesh>
   );
 }
---
+++ src/components/Panorama.js
@@ -0,0 +1,37 @@
+import { useLoader } from "@react-three/fiber";
+import * as THREE from "three";
+import Box from "./Box";
+import { useState } from "react";
+const backgrounds = [
+  {
+    id: 1,
+    url: "/mountain.jpeg",
+  },
+  {
+    id: 2,
+    url: "/beach.jpeg",
+  },
+];
+
+export default function Panorama() {
+  const [activeBackground, setActiveBackground] = useState(1);
+  const { url } = backgrounds.find(({ id }) => id === activeBackground);
+  const background = useLoader(THREE.TextureLoader, url);
+  return (
+    <group>
+      <mesh>
+        <sphereGeometry args={[500, 60, 40]} />
+        <meshBasicMaterial map={background} side={THREE.BackSide} />
+      </mesh>
+      +{" "}
+      <group
+        onClick={(e) => {
+          e.stopPropagation();
+          setActiveBackground(activeBackground === 1 ? 2 : 1);
+        }}
+      >
+        <Box />
+      </group>
+    </group>
+  );
+}
