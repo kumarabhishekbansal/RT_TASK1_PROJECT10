7da9bd3ef8d3c6b2d3132d49b51582dc63001c4f
kumarabhishekbansal <kumarabhishekbansal>
2024-04-01 08:35:37 +0530

    canvas add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
+import Box from "./components/Box";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="Canvas-container">
+      <Canvas>
+        <Box />
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+      </Canvas>
     </div>
   );
 }
-
-export default App;
